5b8ca9eb528dac13593ed073ddf8ab9053c5874e
SK-aped <som.kherwa@momentoustech.com>
2022-03-16 19:47:39 +0530

    initial commint

---
+++ dynamicLinks/src/main/java/com/momentous/dynamicLinks/DynamicLinksApplication.java
@@ -0,0 +1,13 @@
+package com.momentous.dynamicLinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicLinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicLinksApplication.class, args);
+	}
+
+}
---
+++ dynamicLinks/src/main/java/com/momentous/dynamicLinks/controller/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.momentous.dynamicLinks.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        return "welcome"; // view
+    }
+}
